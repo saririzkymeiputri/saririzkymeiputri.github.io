4675f1d774cd84cf87c181e5a1c77d7f7f7e11cb
saririzkymeiputri <saririzkymeiputri@gmail.com>
2025-04-21 11:16:37 +0700

    Add files via upload

--- js/datacard.js
+++ js/datacard.js
@@ -9,13 +9,13 @@ const Formatvideopopuler = [
       title: "AVI (Audio Video Interleave)",
       url: "https://drive.google.com/file/d/1VWOru7lxMIw97zSKR8bWStPLCXnqvPV-/view?usp=sharing",
       description:
-        "AVI merupakan salah satu format video yang paling dikenal dan banyak digunakan dalam berbagai media, terutama sebagai format standar untuk film dan video. Format ini dikembangkan oleh Microsoft dan kerap digunakan sebagai format default pada perangkat seperti handycam maupun kamera digital. AVI mendukung beragam jenis kompresi melalui integrasi dengan berbagai codec seperti DivX dan XVID, yang memungkinkan fleksibilitas dalam penyajian video. Kelebihan dari format ini adalah kualitas gambar yang lebih tajam dibandingkan ASF, namun memiliki keterbatasan dalam resolusi gambar dan kualitas video sangat bergantung pada jenis codec yang digunakan. AVI juga kurang optimal untuk video dengan gerakan penuh (full motion)."
+        "AVI merupakan salah satu format video yang paling dikenal dan banyak digunakan dalam berbagai media, terutama sebagai format standar untuk film dan video. Format ini dikembangkan oleh Microsoft dan kerap digunakan sebagai format default pada perangkat seperti handycam maupun kamera digital. AVI mendukung beragam jenis kompresi melalui integrasi dengan berbagai codec seperti DivX dan XVID, yang memungkinkan fleksibilitas dalam penyajian video. Kelebihan dari format ini adalah kualitas gambar yang lebih tajam dibandingkan ASF, namun memiliki keterbatasan dalam resolusi gambar dan kualitas video sangat bergantung pada jenis codec yang digunakan. AVI juga kurang optimal untuk video dengan gerakan penuh (full motion). size:9,53 MB"
     },
     {
         title: "MP4",
         url: "https://drive.google.com/file/d/1DLgjz9CEAnRu9hJu7fnsxGM_Mu3AiVrf/view?usp=sharing",
         description:
-          "MP4 adalah salah satu format video paling populer secara global. Kepopulerannya semakin meningkat berkat platform seperti YouTube, yang mengadopsi MP4 sebagai format standar untuk setiap video yang ditampilkan. Sebelumnya, YouTube menggunakan format FLV, namun seiring dengan transisi ke pemutar video berbasis HTML5, dukungan terhadap Flash mulai ditinggalkan dan MP4 menjadi pilihan utama. Format ini dapat diputar di hampir semua perangkat dan sistem operasi, serta mendukung integrasi dengan elemen interaktif seperti grafik 3D, menu navigasi, dan fitur lainnya. MP4 mampu mengompres video dan audio menjadi ukuran file yang kecil tanpa mengorbankan kompatibilitas, meskipun kompresi tinggi dapat menurunkan kualitas audio dan video, serta terkadang mengalami kendala sinkronisasi."
+          "MP4 adalah salah satu format video paling populer secara global. Kepopulerannya semakin meningkat berkat platform seperti YouTube, yang mengadopsi MP4 sebagai format standar untuk setiap video yang ditampilkan. Sebelumnya, YouTube menggunakan format FLV, namun seiring dengan transisi ke pemutar video berbasis HTML5, dukungan terhadap Flash mulai ditinggalkan dan MP4 menjadi pilihan utama. Format ini dapat diputar di hampir semua perangkat dan sistem operasi, serta mendukung integrasi dengan elemen interaktif seperti grafik 3D, menu navigasi, dan fitur lainnya. MP4 mampu mengompres video dan audio menjadi ukuran file yang kecil tanpa mengorbankan kompatibilitas, meskipun kompresi tinggi dapat menurunkan kualitas audio dan video, serta terkadang mengalami kendala sinkronisasi. SIZE: 8,74 MB"
       },
     {
         title: "MPG atau MPEG",
@@ -26,13 +26,13 @@ const Formatvideopopuler = [
         title: "WEBM",
         url: "https://drive.google.com/file/d/1emQZQmk7up92mld9J6lFrfZrUE3vhwnz/view?usp=sharing",
         description:
-          "WEBM merupakan salah satu format video yang semakin populer dalam lingkungan internet, terutama sejak diperkenalkannya standar HTML5. Format ini menggunakan codec video VP8 atau VP9 serta audio Vorbis atau Opus, dan dirancang untuk menyediakan pemutaran video berkualitas tinggi dengan efisiensi performa yang baik di web. Format ini menggunakan wadah Matroska yang juga digunakan oleh MKV. Namun tidak semua perangkat portabel mendukung WEBM, dan beberapa browser juga belum sepenuhnya kompatibel."
+          "WEBM merupakan salah satu format video yang semakin populer dalam lingkungan internet, terutama sejak diperkenalkannya standar HTML5. Format ini menggunakan codec video VP8 atau VP9 serta audio Vorbis atau Opus, dan dirancang untuk menyediakan pemutaran video berkualitas tinggi dengan efisiensi performa yang baik di web. Format ini menggunakan wadah Matroska yang juga digunakan oleh MKV. Namun tidak semua perangkat portabel mendukung WEBM, dan beberapa browser juga belum sepenuhnya kompatibel. size: 6,88 MB"
     },
     {
         title: "3GP",
         url: "https://drive.google.com/file/d/1kGjnkaFtpYx2u22ZjPJO2vBuEgvbIYED/view?usp=sharing",
         description:
-          "3GP adalah format video yang dikembangkan oleh Third Generation Partnership Project (3GPP), dan dirancang secara khusus untuk perangkat mobile seperti ponsel. Format ini dibuat dengan tujuan mengurangi ukuran file agar dapat dikirim dan diputar dengan mudah melalui jaringan seluler. Karena ukurannya yang kecil, 3GP menjadi sangat populer di era awal 2000-an. Namun, kualitas gambar dari format ini cenderung rendah dan tidak cocok untuk kebutuhan distribusi video berkualitas tinggi."
+          "3GP adalah format video yang dikembangkan oleh Third Generation Partnership Project (3GPP), dan dirancang secara khusus untuk perangkat mobile seperti ponsel. Format ini dibuat dengan tujuan mengurangi ukuran file agar dapat dikirim dan diputar dengan mudah melalui jaringan seluler. Karena ukurannya yang kecil, 3GP menjadi sangat populer di era awal 2000-an. Namun, kualitas gambar dari format ini cenderung rendah dan tidak cocok untuk kebutuhan distribusi video berkualitas tinggi. size:8,73 MB"
       },
       {
         title: "MKV (Matroska Video)",
@@ -48,7 +48,7 @@ const Formatvideopopuler = [
     {
         title: "FLV",
         url: "https://drive.google.com/file/d/1bF5cqED2Jw_xqmTdKL-dHbdkJXM_CKg8/view?usp=sharing",
-        description: "FLV (Flash Video) adalah format video yang banyak digunakan di awal 2010-an, terutama untuk streaming di platform seperti YouTube. Format ini mampu menampilkan video dengan kualitas yang cukup baik meski ukurannya kecil, sehingga cocok untuk komputer dengan spesifikasi rendah. FLV banyak dipakai untuk film, video pendek, dan anime di masa lalu. Namun, pembuatan kontennya relatif memakan waktu, dan halaman web dengan video FLV sering kali memerlukan waktu loading lebih lama dibanding format modern. Kini, penggunaannya mulai tergantikan oleh format yang lebih efisien dan kompatibel dengan perangkat terkini."
+        description: "FLV (Flash Video) adalah format video yang banyak digunakan di awal 2010-an, terutama untuk streaming di platform seperti YouTube. Format ini mampu menampilkan video dengan kualitas yang cukup baik meski ukurannya kecil, sehingga cocok untuk komputer dengan spesifikasi rendah. FLV banyak dipakai untuk film, video pendek, dan anime di masa lalu. Namun, pembuatan kontennya relatif memakan waktu, dan halaman web dengan video FLV sering kali memerlukan waktu loading lebih lama dibanding format modern. Kini, penggunaannya mulai tergantikan oleh format yang lebih efisien dan kompatibel dengan perangkat terkini. size: 8,64 MB"
     },
     {
         title: "MOV",
@@ -129,17 +129,17 @@ const AudioFormats = [
   {
     "title": "WAV",
     "url": "https://drive.google.com/file/d/1_qEEwridhnSxxZ2G2wk-3_jvPBIu5Mef/view?usp=sharing",
-    "description": "Format WAV merupakan format audio tidak terkompresi yang banyak digunakan dalam industri audio profesional. WAV (Waveform Audio File Format) menempati posisi utama sebagai format audio tidak terkompresi yang paling umum digunakan dalam proses perekaman, pencampuran, serta mastering. Format ini memberikan fleksibilitas tinggi dalam hal kedalaman bit dan laju pengambilan sampel, meskipun dalam praktiknya, laju pengambilan sampel di atas 48kHz jarang diperlukan."
+    "description": "Format WAV merupakan format audio tidak terkompresi yang banyak digunakan dalam industri audio profesional. WAV (Waveform Audio File Format) menempati posisi utama sebagai format audio tidak terkompresi yang paling umum digunakan dalam proses perekaman, pencampuran, serta mastering. Format ini memberikan fleksibilitas tinggi dalam hal kedalaman bit dan laju pengambilan sampel, meskipun dalam praktiknya, laju pengambilan sampel di atas 48kHz jarang diperlukan. size:54,6 MB"
   },
   {
     "title": "AIFF",
     "url": "https://drive.google.com/file/d/1YTGhzuo_0zxN1UCOUyo42wYX592Ylk5B/view?usp=sharing",
-    "description": "AIFF (Audio Interchange File Format) merupakan format berkas audio profesional berbasis PCM (Pulse-Code Modulation) yang tidak mengalami kompresi. Format AIFF dikembangkan oleh Apple sebagai standar untuk menyimpan audio digital dengan kualitas tinggi tanpa kompresi, sehingga tidak ada data suara yang hilang dalam proses penyimpanannya. Artinya, AIFF mempertahankan integritas suara secara utuh, sama seperti format WAV."
+    "description": "AIFF (Audio Interchange File Format) merupakan format berkas audio profesional berbasis PCM (Pulse-Code Modulation) yang tidak mengalami kompresi. Format AIFF dikembangkan oleh Apple sebagai standar untuk menyimpan audio digital dengan kualitas tinggi tanpa kompresi, sehingga tidak ada data suara yang hilang dalam proses penyimpanannya. Artinya, AIFF mempertahankan integritas suara secara utuh, sama seperti format WAV. 54,6 MB"
   },
   {
     "title": "MP3",
     "url": "https://drive.google.com/file/d/1KYcZY1t-8ZdxzohOlGNCsvlLMgVRNZHA/view?usp=sharing",
-    "description": "MP3 (MPEG-1 Audio Layer III) adalah format audio paling populer untuk distribusi musik digital karena mampu mengompresi ukuran file secara signifikan tanpa mengorbankan kualitas suara secara mencolok. MP3 menggunakan kompresi lossy yang menghilangkan data audio yang dianggap tidak terlalu terdengar oleh manusia, sehingga lebih efisien dan banyak digunakan di perangkat portabel, layanan streaming, dan penyimpanan audio digital."
+    "description": "MP3 (MPEG-1 Audio Layer III) adalah format audio paling populer untuk distribusi musik digital karena mampu mengompresi ukuran file secara signifikan tanpa mengorbankan kualitas suara secara mencolok. MP3 menggunakan kompresi lossy yang menghilangkan data audio yang dianggap tidak terlalu terdengar oleh manusia, sehingga lebih efisien dan banyak digunakan di perangkat portabel, layanan streaming, dan penyimpanan audio digital.size: 4.95 MB"
   },
   {
     "title": "DSD",
@@ -149,12 +149,12 @@ const AudioFormats = [
   {
     "title": "MP4",
     "url": "https://drive.google.com/file/d/1L0xmsVDei59gRwyf2nLmARdb5AWkASv8/view?usp=sharing",
-    "description": "Format audio MP4 sering kali disalahartikan sebagai versi yang lebih baik dari MP3, padahal keduanya merupakan format yang berbeda baik dari segi struktur maupun fungsi. MP4 merupakan format media yang bersifat komprehensif karena mampu menyimpan tidak hanya audio, tetapi juga video dan elemen media lainnya. Tidak seperti MP3 yang fokus pada audio saja, MP4 menyimpan data dalam bentuk kontainer, bukan dalam bentuk kode langsung, sehingga memerlukan codec tertentu untuk dapat memproses dan memutar isinya secara tepat."
+    "description": "Format audio MP4 sering kali disalahartikan sebagai versi yang lebih baik dari MP3, padahal keduanya merupakan format yang berbeda baik dari segi struktur maupun fungsi. MP4 merupakan format media yang bersifat komprehensif karena mampu menyimpan tidak hanya audio, tetapi juga video dan elemen media lainnya. Tidak seperti MP3 yang fokus pada audio saja, MP4 menyimpan data dalam bentuk kontainer, bukan dalam bentuk kode langsung, sehingga memerlukan codec tertentu untuk dapat memproses dan memutar isinya secara tepat. size: 5,03 MB"
   },
   {
     "title": "AAC",
     "url": "https://drive.google.com/file/d/1zI_BWlr-zbyMJxTCiACJITXUMzxjD4jB/view?usp=sharing",
-    "description": "AAC (Advanced Audio Coding) merupakan format berkas audio yang dirancang untuk menghasilkan kualitas suara yang tinggi melalui teknik pengodean yang lebih canggih dibandingkan format konvensional. Meskipun tidak mencapai tingkat popularitas seperti MP3 dalam distribusi musik digital, AAC tetap memiliki peran penting sebagai format audio standar pada berbagai perangkat utama, seperti ponsel pintar dan konsol permainan."
+    "description": "AAC (Advanced Audio Coding) merupakan format berkas audio yang dirancang untuk menghasilkan kualitas suara yang tinggi melalui teknik pengodean yang lebih canggih dibandingkan format konvensional. Meskipun tidak mencapai tingkat popularitas seperti MP3 dalam distribusi musik digital, AAC tetap memiliki peran penting sebagai format audio standar pada berbagai perangkat utama, seperti ponsel pintar dan konsol permainan. size:5,07 MB"
   },
   {
     "title": "AMR",
@@ -167,9 +167,9 @@ const AudioFormats = [
     "description": "FLAC (Free Lossless Audio Codec) adalah format audio kompresi lossless yang mampu mengurangi ukuran file tanpa kehilangan kualitas suara sama sekali. Format ini menjadi favorit di kalangan audiophile karena menjaga kualitas asli dari rekaman suara. FLAC juga mendukung metadata dan streaming, serta mulai banyak didukung oleh pemutar audio modern."
   },
   {
-    "title": "MP2",
+    "title": "DFF",
     "url": "https://drive.google.com/file/d/1kOI8cIYdlJlWCqH2n4Y0bJM4dGT4Rjge/view?usp=sharing",
-    "description": "MP2 (MPEG-1 Audio Layer II) adalah pendahulu dari MP3 dan masih digunakan secara luas dalam siaran radio digital dan televisi karena kestabilannya dalam kondisi transmisi. Meskipun kualitas kompresinya lebih rendah dibanding MP3, MP2 tetap andal dalam lingkungan penyiaran yang membutuhkan latensi rendah dan keandalan tinggi."
+    "description": "Format DFF adalah salah satu format file audio beresolusi tinggi yang digunakan untuk menyimpan data DSD (Direct Stream Digital). Format ini biasa digunakan dalam dunia audio profesional dan oleh para audiofil yang menginginkan kualitas suara setinggi mungkin. size:2,5 MB"
   },
   {
     "title": "M4A",
@@ -188,37 +188,37 @@ const GambarFormats = [
     {
       "title": "PNG",
       "url": "https://drive.google.com/file/d/1_uOaXe9NHcrx97WCwM9jJVXAv7SMwktc/view?usp=sharing",
-      "description": "PNG adalah format gambar lossless yang mendukung transparansi dan sangat cocok digunakan dalam desain grafis serta web. Format ini mampu mempertahankan kualitas gambar meskipun disimpan berulang kali, dan tidak mengalami degradasi seperti JPEG. PNG sering digunakan untuk gambar dengan latar belakang transparan atau elemen grafis seperti ikon dan logo."
+      "description": "PNG adalah format gambar lossless yang mendukung transparansi dan sangat cocok digunakan dalam desain grafis serta web. Format ini mampu mempertahankan kualitas gambar meskipun disimpan berulang kali, dan tidak mengalami degradasi seperti JPEG. PNG sering digunakan untuk gambar dengan latar belakang transparan atau elemen grafis seperti ikon dan logo. size:195kb"
     },
     {
       "title": "GIF",
       "url": "https://drive.google.com/file/d/1zduqCfHplF2nIPi8HmOclKO0YYaVAKrk/view?usp=sharing",
-      "description": "GIF merupakan format gambar yang mendukung animasi dan transparansi, namun hanya terbatas pada 256 warna. Meskipun kualitas warnanya terbatas, GIF sangat populer untuk pembuatan animasi ringan atau meme di media sosial karena ukurannya kecil dan dapat berjalan otomatis."
+      "description": "GIF merupakan format gambar yang mendukung animasi dan transparansi, namun hanya terbatas pada 256 warna. Meskipun kualitas warnanya terbatas, GIF sangat populer untuk pembuatan animasi ringan atau meme di media sosial karena ukurannya kecil dan dapat berjalan otomatis. size: 252kb"
     },
     {
       "title": "RAW",
       "url": "https://drive.google.com/file/d/14ZAXTKisD9rwroDwCFvzlhpAAWD3xjP6/view?usp=sharing",
-      "description": "RAW adalah file dengan kualitas tertinggi yang tersedia untuk pengambilan gambar dan penyimpanan, karena mengandung detail paling banyak. Tidak ada kompresi atau konversi, yang dapat menurunkan kualitas gambar dan memengaruhi jenis file raster lainnya."
+      "description": "RAW adalah file dengan kualitas tertinggi yang tersedia untuk pengambilan gambar dan penyimpanan, karena mengandung detail paling banyak. Tidak ada kompresi atau konversi, yang dapat menurunkan kualitas gambar dan memengaruhi jenis file raster lainnya. size: 425 kb"
     },
     {
       "title": "JPG",
-      "url": "https://drive.google.com/file/d/1caCsuc4KH-mlP_ny4gOfI1eQLNWly5EM/view?usp=sharing",
-      "description": "JPG atau JPEG adalah format gambar yang menggunakan kompresi lossy, sehingga ukuran file menjadi lebih kecil namun dapat mengurangi kualitas gambar. Format ini sangat umum digunakan untuk menyimpan foto digital dan gambar dengan gradasi warna yang kompleks."
+      "url": "https://drive.google.com/file/d/10QHd4eWawrcyH5lTnSetfCO58rP36Mt-/view?usp=sharing",
+      "description": "JPG atau JPEG adalah format gambar yang menggunakan kompresi lossy, sehingga ukuran file menjadi lebih kecil namun dapat mengurangi kualitas gambar. Format ini sangat umum digunakan untuk menyimpan foto digital dan gambar dengan gradasi warna yang kompleks.size: 149kb"
     },
     {
       "title": "PSD",
       "url": "https://drive.google.com/file/d/1dLNEIcvE7qTGzz5V9LbZUZUQklIQ_vU1/view?usp=sharing",
-      "description": "PSD adalah format gambar yang dikembangkan oleh Adobe Photoshop, mendukung penyimpanan berbagai elemen desain seperti layer, efek, dan teks. Format ini sangat cocok digunakan untuk keperluan editing lanjutan karena memungkinkan pengguna untuk memodifikasi elemen desain secara fleksibel."
+      "description": "PSD adalah format gambar yang dikembangkan oleh Adobe Photoshop, mendukung penyimpanan berbagai elemen desain seperti layer, efek, dan teks. Format ini sangat cocok digunakan untuk keperluan editing lanjutan karena memungkinkan pengguna untuk memodifikasi elemen desain secara fleksibel. size: 556kb"
     },
     {
       "title": "BMP",
-      "url": "https://drive.google.com/file/d/1pOuoZrRyww01F1KoNRyH87eC9XhZtZ12/view?usp=sharing",
-      "description": "BMP adalah format gambar raster tanpa kompresi yang menyimpan setiap piksel secara detail, menghasilkan kualitas gambar tinggi namun dengan ukuran file yang besar. Format ini biasanya digunakan dalam lingkungan sistem operasi Windows dan bukan pilihan utama untuk web karena tidak efisien."
+      "url": "https://drive.google.com/file/d/1qC7IveCpBZeigjCP8TEs3SkxeuG1fo-3/view?usp=sharing",
+      "description": "BMP adalah format gambar raster tanpa kompresi yang menyimpan setiap piksel secara detail, menghasilkan kualitas gambar tinggi namun dengan ukuran file yang besar. Format ini biasanya digunakan dalam lingkungan sistem operasi Windows dan bukan pilihan utama untuk web karena tidak efisien. size: 509 kb"
     },
     {
       "title": "EPS",
       "url": "https://drive.google.com/file/d/1hqzhHcMRmk6EBYZH4eNNoliGlnkbMC-Z/view?usp=sharing",
-      "description": "EPS adalah format file vektor yang umum digunakan dalam dunia percetakan dan desain profesional. Format ini dapat menyimpan teks dan grafik dalam bentuk yang bisa diperbesar tanpa kehilangan kualitas, menjadikannya ideal untuk logo dan ilustrasi cetak."
+      "description": "EPS adalah format file vektor yang umum digunakan dalam dunia percetakan dan desain profesional. Format ini dapat menyimpan teks dan grafik dalam bentuk yang bisa diperbesar tanpa kehilangan kualitas, menjadikannya ideal untuk logo dan ilustrasi cetak.size: 1,05 MB"
     },
     {
       "title": "ODD",
